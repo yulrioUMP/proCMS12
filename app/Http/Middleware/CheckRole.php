b2517da4ecbbff8a2f3d585aec9c3006b69a4064
Yulrio Brianorman <33221012@std.stei.itb.ac.id>
2022-05-30 17:13:31 +0700

    All

---
+++ app/Http/Middleware/CheckRole.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class CheckRole
+{
+    public function handle(Request $request, Closure $next, ...$roles)
+    {
+        if (in_array($request->user()->role, $roles)) {
+            return $next($request);
+        }
+        return redirect('/home');
+    }
+}
